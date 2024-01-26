7fa63e4a324b5731c17df58297a41597cf5069f1
cptalpdeniz <cptalpdeniz@gmail.com>
2024-01-26 08:35:39 -0800

    add to-do list

--- Air Anatolia VA Assignment Creator/Program.cs
+++ Air Anatolia VA Assignment Creator/Program.cs
@@ -2,6 +2,13 @@
 
 namespace Air_Anatolia_VA_Assignment_Creator
 {
+    /*
+     * TO-DO
+     * Add cargo vs passenger
+     * fix empty input from user because humans?
+     * if count is zero program patliyo
+     * 
+     */
     internal class Program
     {
         public static int[] ConvertStringToDaysOfWeekArray(string input)
